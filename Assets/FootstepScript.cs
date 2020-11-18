d5af49b1de1dea6e1508fd9ccd9e2a0bbe5d8eed
miguel-music <miguel.ines.music@gmail.com>
2020-11-18 01:36:37 +0000

    Wwise implemented
    Wwise implemented
    Footstep spatial attenuation applied (to be worked)

---
+++ Assets/FootstepScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepScript : MonoBehaviour
+{
+    public AK.Wwise.Event FootstepSound1;
+    // Use this for initialization.
+    void PlayFootstepSound1()
+    {
+        FootstepSound1.Post(gameObject);
+    }
+    public AK.Wwise.Event FootstepSound2;
+    // Use this for initialization.
+    void PlayFootstepSound2()
+    {
+        FootstepSound2.Post(gameObject);
+    }
+    public AK.Wwise.Event FootstepSound3;
+    // Use this for initialization.
+    void PlayFootstepSound3()
+    {
+        FootstepSound3.Post(gameObject);
+    }
+}
