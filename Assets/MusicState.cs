fc22a309aaa45744a870e4ee10e0b75435fd36a1
miguel-music <miguel.ines.music@gmail.com>
2020-12-07 23:32:10 +0000

    implemented overworld music in both states

---
+++ Assets/MusicState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicState : MonoBehaviour
+{
+
+    public string StateGroup = "Overworld";
+    public string State = "Tense";
+    public string ExitState = "NonTense";
+    public GameObject Player;
+    public bool Debug_Enabled;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (Debug_Enabled)
+        {
+            //        Debug.Log(State + "state set"); 
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            AkSoundEngine.SetState(StateGroup, State);
+        }
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (Debug_Enabled)
+        {
+            //        Debug.Log(ExitState + "state set"); 
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            AkSoundEngine.SetState(StateGroup, ExitState);
+        }
+    }
+}
