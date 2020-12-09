257ace872ff60fc4bd34e1e1004ad4b203b7ae11
Wiloux <54811449+Wiloux@users.noreply.github.com>
2020-12-09 20:45:24 +0100

    Pushed Options menu

---
+++ Assets/MusicSlider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSlider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider Slider1; 
+    public Slider Slider2;
+
+    public Image Img1;
+    public Image Img2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Img1.fillAmount = Slider1.value;
+        Img2.fillAmount = Slider2.value;   
+    }
+}
