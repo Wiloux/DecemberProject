7df8e0721db4634719aec06badb2f38b4b7a8af7
miguel-music <miguel.ines.music@gmail.com>
2020-12-09 22:51:38 +0000

    check descrp
    sliders working
    spatialized orb
    amb goes until the end
    spatialized and lowered big orb

---
+++ Assets/RTPCStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTPCStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.SetRTPCValue("MusicVolume", 8);
+        AkSoundEngine.SetRTPCValue("SFXVolume", 8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
