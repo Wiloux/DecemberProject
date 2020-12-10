7be1c1cf6660e5889a3ef902ecad15742761de0c
miguel-music <miguel.ines.music@gmail.com>
2020-12-10 00:36:01 +0000

    some smaller tweaks

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -219,7 +219,7 @@ public class Enemy : MonoBehaviour
                             newtv.survivor = CurrentTransformPlayer.PlayerScript;
                         }
                       
-                       // AkSoundEngine.PostEvent("IsBeingCorrupted", gameObject);
+                        AkSoundEngine.PostEvent("IsBeingCorrupted", gameObject);
                         currentState = States.Pause;
 
                         foreach (SpottedSurvior Player in Players)
